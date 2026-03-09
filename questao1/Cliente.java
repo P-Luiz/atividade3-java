6f64b57d4489ab9e932dfc2af6e44ff1223f15fb
P-Luiz <pdrlzluiz@gmail.com>
2026-03-09 19:32:03 -0300

    finalizando a questao1

--- questao1/Cliente.java
+++ questao1/Cliente.java
@@ -4,26 +4,34 @@ import java.util.Scanner;
 
 public class Cliente{
     String nome;
-    int telefone;
-    int cpf;
+    long telefone;
+    long cpf;
     char sexo;
     float comprou;
 
     void cadastrar(Scanner leia){
+        leia.nextLine();
         System.out.println("digite o nome do cliente: ");
         this.nome = leia.nextLine();
         System.out.println("digite o telefone do cliente: ");
-        this.telefone = leia.nextInt();
+        this.telefone = leia.nextLong();
         System.out.println("digite o cpf do cliente");
-        this.cpf = leia.nextInt();
+        this.cpf = leia.nextLong();
+        System.out.println("digite o sexo do cliente \n M - masculino \n F - feminino");
+        this.sexo = leia.next().charAt(0); 
     }
 
-    void excluir(){
-
+    void vender(Scanner leia){
+        System.out.printf("digite o valor da compra de %s: \n",this.nome);
+        this.comprou = (this.comprou + leia.nextFloat());
     }
 
-    void modificar(){
-
+    void exibir(){
+        System.out.println("\n----- DADOS DO CLIENTE -----");
+        System.out.println("NOME: "+this.nome);
+        System.out.println("TELEFONE: "+this.telefone);
+        System.out.println("CPF: "+this.cpf);
+        System.out.println("SEXO: "+this.sexo);
+        System.out.println("COMPROU: "+this.comprou);
     }
-
 }
--- questao1/Main.java
+++ questao1/Main.java
@@ -1,7 +1,19 @@
 package questao1;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[]args){
-        
+        Scanner leia = new Scanner(System.in);
+        Cliente cliente1 = new Cliente();
+        Cliente cliente2 = new Cliente();
+        cliente1.cadastrar(leia);
+        cliente2.cadastrar(leia);
+        cliente1.vender(leia);
+        cliente2.vender(leia);
+        cliente1.exibir();
+        cliente2.exibir();
     }
 }
+
+// 1)      Faça uma classe Cliente com 5 atributos e 3 métodos. Instancie (simule) 2 objetos. 
